3edbd088acc8856ed9da9378b3d5804edbfb74a3
Janelle Sjue <janellesjue@gmail.com>
2018-11-08 11:43:43 -0600

    samll changes

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogfromhell@localhost:8889/build-a-blog'
+                                        
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Build-a-blog:couch@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+app.secret_key = 'poiuytrewq'
 
 
 
